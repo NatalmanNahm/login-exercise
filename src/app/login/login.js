8c8de2e01179c275cf17c982999e957ca6e715f3
Natalman <natalnahm@gmail.com>
2023-09-05 14:14:32 -0500

    initial project with the login page.

---
+++ src/app/login/login.js
@@ -0,0 +1,48 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const server = document.getElementById("serverSelect").value;
+    const notes = document.getElementById("notes").value;
+
+
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
+    console.log(`Server: ${server}`);
+    console.log(`Notes: ${notes}`);
+});
+
+// Password visibility toggle
+const passwordInput = document.getElementById("password");
+const passwordToggle = document.getElementById("passwordToggle");
+
+passwordToggle.addEventListener("click", function () {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordToggle.textContent = "👁️";
+    } else {
+        passwordInput.type = "password";
+        passwordToggle.textContent = "🔑";
+    }
+});
+
+// Get the server select element by its ID
+const serverSelect = document.getElementById("serverSelect");
+
+// Define an array of server options
+const serverOptions = ["Choose a Server", "Development", "Test", "Demo", "Customer1", "Customer2"];
+
+
+// Function to populate the server select element
+function populateServerSelect() {
+    for (let i = 0; i < serverOptions.length; i++) {
+        const option = document.createElement("option");
+        option.value = serverOptions[i];
+        option.textContent = serverOptions[i];
+        serverSelect.appendChild(option);
+    }
+}
+
+// Call the function to populate the server select element
+populateServerSelect();
